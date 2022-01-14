587a91c229d6de4f9edbb5dd4748895fd0cdcde1
gabrieldias <gabrielrosadias@gmail.com>
2022-01-14 13:56:56 -0300

    pessistiu?

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "random_id" "instance_id" {
 
 // A single Compute Engine instance
 resource "google_compute_instance" "default" {
- name         = "flask-vm-${random_id.instance_id.hex}"
+ name         = "vm-${random_id.instance_id.hex}"
  machine_type = "f1-micro"
  zone         = "us-west1-a"
 
